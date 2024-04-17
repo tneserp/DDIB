917314be3b01780161b23c58825378523fb999d9
예준성 <306yyy@naver.com>
2024-04-17 17:50:49 +0900

    Feat: S3 경로 도메인 상수화

---
+++ product/src/main/java/com/ddib/product/common/file/constant/S3Domain.java
@@ -0,0 +1,8 @@
+package com.ddib.product.common.file.constant;
+
+public class S3Domain {
+
+    public static final String PRODUCT_DETAIL = "details";
+
+    public static final String PRODUCT_THUMBNAIL = "thumbnail";
+}
