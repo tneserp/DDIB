cc03fc1f9510b289d2a3f3c67864b84b1168bccb
예준성 <306yyy@naver.com>
2024-04-17 17:17:14 +0900

    Feat: 재고 부족에 대한 예외 작성

---
+++ product/src/main/java/com/ddib/product/product/exception/ProductStockShortageException.java
@@ -0,0 +1,13 @@
+package com.ddib.product.product.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ProductStockShortageException extends RuntimeException {
+
+    private static final String MESSAGE = "[PRODUCT ERROR] 해당 상품의 재고 수량이 부족합니다.";
+
+    public ProductStockShortageException() {
+        super(MESSAGE);
+    }
+}
