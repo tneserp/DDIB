ee72f72a0eb786e13d644c2994851dd6ba6c7c54
예준성 <306yyy@naver.com>
2024-04-17 17:18:29 +0900

    Feat: product 도메인 entity 작성

---
+++ product/src/main/java/com/ddib/product/product/domain/Product.java
@@ -0,0 +1,57 @@
+package com.ddib.product.product.domain;
+
+import com.ddib.product.product.exception.ProductStockShortageException;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    private String name;
+
+    private int totalStock;
+
+    private int stock;
+
+    private Timestamp eventDate;
+
+    private int eventStartTime;
+
+    private int eventEndTime;
+
+    private int price;
+
+    private double discount;
+
+    private String thumbnailImage;
+
+    private ProductCategory category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductDetail> details;
+
+    public void updateStock(int amount){
+        isStockShortage(amount);
+        stock -= amount;
+    }
+
+    private void isStockShortage(int amount){
+        if(stock - amount < 0){
+            throw new ProductStockShortageException();
+        }
+    }
+}
---
+++ product/src/main/java/com/ddib/product/product/domain/ProductCategory.java
@@ -0,0 +1,20 @@
+package com.ddib.product.product.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductCategory {
+
+    FASHION("패션"),
+    BEAUTY("뷰티"),
+    FOOD("식품"),
+    APPLIANCE("가전"),
+    SPORTS("스포츠"),
+    LIVING("생활용품"),
+    PET("반려용품"),
+    TRAVEL("여행");
+
+    private final String meaning;
+}
