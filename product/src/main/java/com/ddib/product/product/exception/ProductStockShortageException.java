553a934981524102303820f9dec584f95d36020d
예준성 <306yyy@naver.com>
2024-04-19 10:03:30 +0900

    Refactor: 에러 메세지 수정

--- product/src/main/java/com/ddib/product/product/exception/ProductStockShortageException.java
+++ product/src/main/java/com/ddib/product/product/exception/ProductStockShortageException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class ProductStockShortageException extends RuntimeException {
 
-    private static final String MESSAGE = "[PRODUCT ERROR] 해당 상품의 재고 수량이 부족합니다.";
+    private static final String MESSAGE = "[ERROR] 해당 상품의 재고 수량이 부족합니다.";
 
     public ProductStockShortageException() {
         super(MESSAGE);
