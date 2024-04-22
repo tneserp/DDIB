fe4d0d5b7b70f1a8370bd1606d436f00e6cce57b
예준성 <306yyy@naver.com>
2024-04-22 16:27:36 +0900

    Rename: NotExist -> NotFound 로 예외명 변경

--- product/src/main/java/com/ddib/product/product/exception/ProductNotFoundException.java
+++ product/src/main/java/com/ddib/product/product/exception/ProductNotFoundException.java
@@ -1,10 +1,10 @@
 package com.ddib.product.product.exception;
 
-public class ProductNotExistException extends RuntimeException {
+public class ProductNotFoundException extends RuntimeException {
 
     private static final String MESSAGE = "[ERROR] 해당 상품은 존재하지 않습니다.";
 
-    public ProductNotExistException(){
+    public ProductNotFoundException(){
         super(MESSAGE);
     }
 }
