44983497b515f51c3310a2fb825e3e2b303ee1eb
예준성 <306yyy@naver.com>
2024-04-22 16:28:00 +0900

    Feat: 유저 및 판매자 조회 시 미조회에 대한 예외 작성

---
+++ product/src/main/java/com/ddib/product/user/exception/SellerNotFoundException.java
@@ -0,0 +1,6 @@
+package com.ddib.product.user.exception;
+
+public class SellerNotFoundException extends RuntimeException {
+
+    private static final String MESSAGE = "[ERROR] 해당 상품은 존재하지 않습니다.";
+}
