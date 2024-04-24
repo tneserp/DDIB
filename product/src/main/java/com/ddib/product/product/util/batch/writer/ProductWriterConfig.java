69fe961dfefa057dc6f3e141d8a91aee8bfc8372
예준성 <306yyy@naver.com>
2024-04-24 15:53:44 +0900

    Feat: 매 정각시간에 따라 24시간, 1시간 남은 물품 알림 위한 전처리 배치 구현
    - 24시간, 1시간 물품을 구분할 수 있는 칼럼 추가 필요

---
+++ product/src/main/java/com/ddib/product/product/util/batch/writer/ProductWriterConfig.java
@@ -0,0 +1,29 @@
+package com.ddib.product.product.util.batch.writer;
+
+import com.ddib.product.product.domain.FavoriteProduct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ProductWriterConfig {
+
+    @StepScope
+    @Bean
+    public ItemWriter<FavoriteProduct> productAlarmWriter() {
+        return chunk -> {
+            for (FavoriteProduct favoriteProduct : chunk.getItems()) {
+                try {
+//                    alarmService.fcm알람전송호출(favoriteProduct);
+                } catch (Exception e) {
+                    log.info("productAlarmWriter ERROR : {} ", e.getMessage());
+                }
+            }
+        };
+    }
+}
