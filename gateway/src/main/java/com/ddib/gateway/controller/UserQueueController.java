4420005a1e5d6fe5b2c414207153d3822e090d1c
jaehyeon <hjaehyeon98@gmail.com>
2024-04-24 17:51:28 +0900

    Chore: webflux, redis, validation 라이브러리 빌드

---
+++ gateway/src/main/java/com/ddib/gateway/controller/UserQueueController.java
@@ -0,0 +1,11 @@
+package com.ddib.gateway.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/queue")
+public class UserQueueController {
+
+}
