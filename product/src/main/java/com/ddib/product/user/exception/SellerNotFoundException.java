dc57947694fef79e1702e41dfece7931d5713c9b
예준성 <306yyy@naver.com>
2024-04-25 11:08:52 +0900

    Refactor: 에러 메세지 수정

--- product/src/main/java/com/ddib/product/user/exception/SellerNotFoundException.java
+++ product/src/main/java/com/ddib/product/user/exception/SellerNotFoundException.java
@@ -2,7 +2,7 @@ package com.ddib.product.user.exception;
 
 public class SellerNotFoundException extends RuntimeException {
 
-    private static final String MESSAGE = "[ERROR] 해당 상품은 존재하지 않습니다.";
+    private static final String MESSAGE = "[ERROR] 해당 판매자는 존재하지 않습니다.";
 
     public SellerNotFoundException() {
         super(MESSAGE);
