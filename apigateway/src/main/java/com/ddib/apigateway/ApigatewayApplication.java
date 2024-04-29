f83dbd12ae674adb47f967eb6990413e0c34aa38
김유나 <kn9012@naver.com>
2024-04-29 15:33:34 +0900

    Feat: Gateway Route 설정

--- apigateway/src/main/java/com/ddib/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/ddib/apigateway/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ddib.gateway;
+package com.ddib.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+public class ApigatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
 }
