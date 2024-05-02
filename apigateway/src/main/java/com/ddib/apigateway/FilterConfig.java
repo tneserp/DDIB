d72bdd0548cb945f38b7e70dd56a0dc500d332d2
김유나 <kn9012@naver.com>
2024-05-02 15:41:03 +0900

    Chore: gateway user uri 수정

--- apigateway/src/main/java/com/ddib/apigateway/FilterConfig.java
+++ apigateway/src/main/java/com/ddib/apigateway/FilterConfig.java
@@ -12,15 +12,15 @@ public class FilterConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/user/**", "/api/oauth2/ddib/kakao")
-                        .uri("http://localhost:8081"))
-                .route(r -> r.path("/api/seller/**", "/api/oauth2/bidd/kakao")
-                        .uri("http://localhost:8085"))
-                .route(r -> r.path("/api/notification/**")
-                        .uri("http://localhost:8084"))
-                .route(r -> r.path("/api/product/**")
-                        .uri("http://localhost:8082"))
-                .route(r -> r.path("/api/payment/**")
-                        .uri("http://localhost:8083"))
+                        .uri("http://ddib-user-service.default.svc.cluster.local:8081"))
+//                .route(r -> r.path("/api/seller/**", "/api/oauth2/bidd/kakao")
+//                        .uri("http://localhost:8085"))
+//                .route(r -> r.path("/api/notification/**")
+//                        .uri("http://localhost:8084"))
+//                .route(r -> r.path("/api/product/**")
+//                        .uri("http://localhost:8082"))
+//                .route(r -> r.path("/api/payment/**")
+//                        .uri("http://localhost:8083"))
                 .build();
     }
 }
