3188449ad103ba556d66491d15927ec2288be579
jaehyeon <hjaehyeon98@gmail.com>
2024-05-03 09:02:17 +0900

    Refactor: 대기열 리팩토링

--- waiting/src/main/java/com/ddib/waiting/controller/UserQueueController.java
+++ waiting/src/main/java/com/ddib/waiting/controller/UserQueueController.java
@@ -5,31 +5,52 @@ import com.ddib.waiting.dto.RankNumberResponse;
 import com.ddib.waiting.dto.RegisterUserResponse;
 import com.ddib.waiting.service.UserQueueService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpCookie;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.Rendering;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/api/v1/queue")
 
 public class UserQueueController { // UserQueueController 클래스 선언
 
     private final UserQueueService userQueueService; // UserQueueService 주입
 
-    // 대기 등록
-    @PostMapping("") // POST 요청 매핑
-    public Mono<RegisterUserResponse> registerUser(@RequestParam(name = "queue", defaultValue = "default") String queue, // 대기열 이름 받아옴 (기본값: default)
-                                                   @RequestParam(name = "user_id") Long userId) { // 사용자 ID 받아옴
-        return userQueueService.registerWaitQueue(queue, userId) // UserQueueService의 대기열 등록 메서드 호출
-                .map(RegisterUserResponse::new); // 등록된 사용자 응답으로 매핑
+    @GetMapping("")
+    public Mono<Rendering> waitingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                           @RequestParam(name = "user_id") Long userId,
+                                           @RequestParam(name = "redirect_url") String redirectUrl,
+                                           ServerWebExchange exchange) {
+        String key = "user-queue-%s-token".formatted(queue);
+        HttpCookie cookieValue = exchange.getRequest().getCookies().getFirst(key);
+        String token = cookieValue == null ? "" : cookieValue.getValue();
+
+        // 1. 입장이 허용되어 page redirect(이동)이 가능한지?
+        return userQueueService.isAllowedByToken(queue, userId, token)
+                .filter(allowed -> allowed)
+                .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build()))
+                .switchIfEmpty(
+                        // 대기 등록. 이미 있으면 웹 페이지에 필요한 데이터 전달
+                        userQueueService.registerWaitQueue(queue, userId)
+                                .onErrorResume(ex ->userQueueService.getRank(queue, userId))
+                                .map(rank -> Rendering.view("waiting-room.html")
+                                        .modelAttribute("number", rank)
+                                        .modelAttribute("userId", userId)
+                                        .modelAttribute("queue", queue)
+                                        .build())
+                );
     }
 
     // 허용 가능한 사용자인지 확인
     @GetMapping("/allowed") // GET 요청 매핑
+    @ResponseBody
     public Mono<AllowedUserResponse> isAllowedUser(@RequestParam(name = "queue", defaultValue = "default") String queue, // 대기열 이름 받아옴 (기본값: default)
                                                    @RequestParam(name = "user_id") Long userId, // 사용자 ID 받아옴
                                                    @RequestParam(name = "token") String token) { // 토큰 받아옴
@@ -38,13 +59,15 @@ public class UserQueueController { // UserQueueController 클래스 선언
     }
 
     @GetMapping("/rank") // GET 요청 매핑
+    @ResponseBody
     public Mono<RankNumberResponse> getRankUser(@RequestParam(name = "queue", defaultValue = "default") String queue, // 대기열 이름 받아옴 (기본값: default)
                                                 @RequestParam(name = "user_id") Long userId) { // 사용자 ID 받아옴
         return userQueueService.getRank(queue, userId) // 사용자의 순위 조회
                 .map(RankNumberResponse::new); // 순위 응답으로 매핑
     }
 
-    @GetMapping("/touch") // GET 요청 매핑
+    @GetMapping("/touch")
+        // GET 요청 매핑
     Mono<?> touch(@RequestParam(name = "queue", defaultValue = "default") String queue, // 대기열 이름 받아옴 (기본값: default)
                   @RequestParam(name = "user_id") Long userId, // 사용자 ID 받아옴
                   ServerWebExchange exchange) { // ServerWebExchange 객체 받아옴
