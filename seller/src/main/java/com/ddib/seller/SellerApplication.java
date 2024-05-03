9ac6cb0a271e799078b7ea9352903b95b4e80fdb
김유나 <kn9012@naver.com>
2024-05-03 11:07:32 +0900

    Fix: Spring Security Principal null 해결

--- seller/src/main/java/com/ddib/seller/SellerApplication.java
+++ seller/src/main/java/com/ddib/seller/SellerApplication.java
@@ -2,9 +2,7 @@ package com.ddib.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class SellerApplication {
 
--- seller/src/main/java/com/ddib/seller/seller/setting/jwt/JWTFilter.java
+++ seller/src/main/java/com/ddib/seller/seller/setting/jwt/JWTFilter.java
@@ -35,7 +35,7 @@ public class JWTFilter extends OncePerRequestFilter {
         String Authorization = request.getHeader("Authorization");
 
         // Authorization 헤더 검증
-        if (Authorization == null || Authorization.startsWith("Bearer ")) {
+        if (Authorization == null || !Authorization.startsWith("Bearer ")) {
             filterChain.doFilter(request, response); // authorization 헤더에 토큰이 없으면 다음 필터로 request, response를 넘겨줌
 
             // 조건이 해당되면 메소드 종료 (필수)
@@ -62,7 +62,7 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         // 토큰에서 email 획득
-        String email = jwtUtil.getUsername(token);
+        String email = jwtUtil.getEmail(token);
 
         // User 엔티티 생성한 후 값 set
         Seller user = Seller.builder()
