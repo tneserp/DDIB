b68edd9a4b05ba270ca36baefaa83d64bd3e9e16
김유나 <kn9012@naver.com>
2024-05-03 17:01:22 +0900

    Add: application-dev.yml, application-prod.yml 파일 추가

--- seller/src/main/java/com/ddib/seller/SellerApplication.java
+++ seller/src/main/java/com/ddib/seller/SellerApplication.java
@@ -2,7 +2,9 @@ package com.ddib.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SellerApplication {
 
