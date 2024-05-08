2ddd99f16509911fa6381b77e543d272c4a8fd72
김유나 <kn9012@naver.com>
2024-05-08 16:49:44 +0900

    Chore: 접속 uri 수정

--- user/src/main/java/com/ddib/user/user/handler/CustomSuccessHandler.java
+++ user/src/main/java/com/ddib/user/user/handler/CustomSuccessHandler.java
@@ -62,7 +62,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         response.addHeader("Authorization", "Bearer " + access);
         log.info("response " + response.getHeader("Authorization"));
-        response.sendRedirect("https://k10c102.p.ssafy.io/");
     }
 
     private Cookie createCookie(String key, String value) {
