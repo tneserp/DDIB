4729477381f90644ce5635a88c050b96cd01ab19
chojayoung <mkwhwkdud@naver.com>
2024-05-10 16:34:44 +0900

    Feat: detail console

--- FrontEnd/DDIB/src/app/_components/ProductOrdered.tsx
+++ FrontEnd/DDIB/src/app/_components/ProductOrdered.tsx
@@ -4,14 +4,33 @@ import styles from "./productOrdered.module.scss";
 import Image from "next/image";
 import { OrderProduct } from "@/app/_types/types";
 import { BiSolidBusiness } from "react-icons/bi";
+import { useEffect } from "react";
 
-export default function ProductOrdered({ productId, thumbnailImage, companyName, name, totalAmount, price, salePrice, status }: OrderProduct) {
+export default function ProductOrdered({
+  productId,
+  thumbnailImage,
+  companyName,
+  name,
+  totalAmount,
+  price,
+  salePrice,
+  status,
+}: OrderProduct) {
+  useEffect(() => {
+    console.log(thumbnailImage);
+  }, []);
+
   return (
     <div className={status === 0 ? styles.mainPink : styles.mainGray}>
       <div className={styles.orderedItem}>
         <div>
           <div className={styles.thumbnail}>
-            <Image src={thumbnailImage} alt="상품썸네일" fill sizes="auto"></Image>
+            <Image
+              src={thumbnailImage}
+              alt="상품썸네일"
+              fill
+              sizes="auto"
+            ></Image>
           </div>
         </div>
         <div>
@@ -29,7 +48,9 @@ export default function ProductOrdered({ productId, thumbnailImage, companyName,
         <div>{price.toLocaleString("ko-KR")}</div>
         <div>{salePrice.toLocaleString("ko-KR")}</div>
       </div>
-      <div className={styles.totalPrice}>{(salePrice * totalAmount).toLocaleString("ko-KR")}</div>
+      <div className={styles.totalPrice}>
+        {(salePrice * totalAmount).toLocaleString("ko-KR")}
+      </div>
     </div>
   );
 }
