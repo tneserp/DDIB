0dd3ebc9167596f9a5a43983200ac5c3eab640a6
chojayoung <mkwhwkdud@naver.com>
2024-05-10 16:18:36 +0900

    Feat: loginPage

--- FrontEnd/DDIB/src/app/(route)/login/oauth2/code/ddib/page.tsx
+++ FrontEnd/DDIB/src/app/(route)/login/oauth2/code/ddib/page.tsx
@@ -4,13 +4,5 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Login() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (router.query) {
-      router.replace("/login/done");
-    }
-  }, []);
-
-  return <>로그인</>;
+  return <>로그인!!</>;
 }
