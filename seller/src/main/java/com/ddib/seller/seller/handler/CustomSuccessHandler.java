a357edfb6fbaf3a6094f5dbd8ce8f240499f49c7
김유나 <kn9012@naver.com>
2024-05-11 23:37:39 +0900

    Chore: 카카오 소셜로그인 후 redirect uri 수정

--- seller/src/main/java/com/ddib/seller/seller/handler/CustomSuccessHandler.java
+++ seller/src/main/java/com/ddib/seller/seller/handler/CustomSuccessHandler.java
@@ -67,7 +67,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         response.addHeader("Authorization", "Bearer " + access);
         log.info("response " + response.getHeader("Authorization"));
 
-        response.sendRedirect("http://k10c102.p.ssafy.io/login/done");
+        response.sendRedirect("https://k10c102.p.ssafy.io/login/done");
     }
 
     private Cookie createCookie(String key, String value) {
