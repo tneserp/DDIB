14507e0bc3101b7ec0873a647482452029e277b7
김유나 <kn9012@naver.com>
2024-05-13 23:39:49 +0900

    Chore: cors config 수정

--- seller/src/main/java/com/ddib/seller/seller/setting/config/CorsMvcConfig.java
+++ seller/src/main/java/com/ddib/seller/seller/setting/config/CorsMvcConfig.java
@@ -20,8 +20,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
                 .exposedHeaders("Authorization")
-                .allowedOrigins("http://"+ releaseHostName+":5173",
-                        "https://"+ releaseHostName, "https://" + releaseHostName + ":5173", "http://192.168.100.134:3000", "http://192.168.100.134:5173")
+                .allowedOrigins("http://"+ releaseHostName+":3001",
+                        "https://"+ releaseHostName, "https://" + releaseHostName + ":3001")
                 .allowCredentials(true);
 
     }
