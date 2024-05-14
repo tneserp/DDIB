0c88969f78bb93cdf3aff26c9b5777beb7982c0a
김유나 <kn9012@naver.com>
2024-05-14 23:07:06 +0900

    Remove: Eureka 라이브러리 주석

--- seller/src/main/java/com/ddib/seller/SellerApplication.java
+++ seller/src/main/java/com/ddib/seller/SellerApplication.java
@@ -2,7 +2,6 @@ package com.ddib.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
