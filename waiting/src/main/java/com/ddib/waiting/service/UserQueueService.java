9e25e46f35ee1fbfebd6d9930cf3f3802f5b1ff2
jaehyeon <tneserp2@gmail.com>
2024-05-14 16:13:49 +0900

    Fix: 대기열 번호 발급 문제 해결

--- waiting/src/main/java/com/ddib/waiting/service/UserQueueService.java
+++ waiting/src/main/java/com/ddib/waiting/service/UserQueueService.java
@@ -60,7 +60,7 @@ public class UserQueueService {
     public Mono<Long> getRank(final String queue, final Long userId) {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()) // 랭크 조회
 //                .defaultIfEmpty(-1L) // 기본값 설정
-                .defaultIfEmpty(1L) // 기본값 설정
+                .defaultIfEmpty(-1L) // 기본값 설정
                 .map(rank -> rank >= 0 ? rank + 1 : rank); // 순위 반환
     }
 
