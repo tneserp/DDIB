fb26437d4ac703f69f6b844b2d4864506cec6d18
김유나 <kn9012@naver.com>
2024-05-16 17:33:43 +0900

    Debug: notification 커밋 되돌리기

--- notification/src/main/java/com/ddib/notification/notification/domain/NotificationText.java
+++ notification/src/main/java/com/ddib/notification/notification/domain/NotificationText.java
@@ -8,7 +8,7 @@ public class NotificationText {
     public static final String CATEGORY_PRODUCT_REGISTRATION_FIRST_TITLE = "님이 구독한 ";
     public static final String CATEGORY_PRODUCT_REGISTRATION_SECOND_TITLE = " 카테고리에 새 상품이 등록됐습니다!";
 
-    public static final String CATEGORY_PRODUCT_REGISTRATION_CONTENT = "띱에 구경하러 오시는건 어떨까요?";
+    public static final String CATEGORY_PRODUCT_REGISTRATION_CONTENT = "띱에 구경하러 와보세요!";
 
     /* 관심 상품 등록 됐을때 : 1시간/24시간 전 */
     public static final String INTEREST_PRODUCT_REMAIN_TIME_FIRST_TITLE = "님이 관심 있는 ";
