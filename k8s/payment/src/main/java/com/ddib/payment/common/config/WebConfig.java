3412f1dee36737f3ec8941f321ae15c31cc429cc
tpwls101 <tpwls101@naver.com>
2024-05-22 14:45:44 +0900

    Feat: cors 추가

---
+++ k8s/payment/src/main/java/com/ddib/payment/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ddib.payment.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("Set-Cookie")
+                .exposedHeaders("Authorization")
+                .allowedOrigins("*")
+                .allowedOriginPatterns("*");
+    }
+}
